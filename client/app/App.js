11ed96cc6701d1f991af8b459740f1ea7fb31ccd
samzevo <samzevo@gmail.com>
2020-07-07 23:45:57 +0530

    configured expo & dockerized

--- client/app/App.js
+++ client/app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hotel App</Text>
+      <Text>Hotel App 2.0</Text>
       <StatusBar style="auto" />
     </View>
   );
