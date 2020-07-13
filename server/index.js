ed2ad42664171dcddc28069e0c79ef037c7d75de
samzevo <samzevo@gmail.com>
2020-07-13 16:48:37 +0530

    added mongo-db docker configurations

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-const app = 1
+const express = require('express')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+
+
+mongoose
+  .connect(
+    'mongodb://mongo:27017/db',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
+app.listen(5000,()=>console.log('listening on 5000'))
