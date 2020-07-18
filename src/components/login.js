33b47a51469e45f93f0627806eaf090d79cb8084
ArunKutty798 <arunkutty798@gmail.com>
2020-07-18 18:37:51 +0530

    added button component

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Input from './input';
+import Button from './button';
 
 function login() {
     return (
       <div>
         
         <Input type='number' />
+        <Input type='password' />
+        <Button type='login' />
         
       </div>
     );
