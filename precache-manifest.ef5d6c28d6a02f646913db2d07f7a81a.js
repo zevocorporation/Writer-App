288718b1f160aa68491ab1ac9ae47da91fb9c51c
samzevo <samzevo@gmail.com>
2020-08-08 10:53:14 +0530

    Updates

---
+++ precache-manifest.ef5d6c28d6a02f646913db2d07f7a81a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e4f15708f137aad396a0262ba69ddc",
+    "url": "/zevocorporation/Writer-App.git/index.html"
+  },
+  {
+    "revision": "2ba334b0f0ca55bff17f",
+    "url": "/zevocorporation/Writer-App.git/static/js/2.f3da0f9a.chunk.js"
+  },
+  {
+    "revision": "ec814dca226ec610acb2358fc67425d6",
+    "url": "/zevocorporation/Writer-App.git/static/js/2.f3da0f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fac048f1b584139981e",
+    "url": "/zevocorporation/Writer-App.git/static/js/main.2c222e71.chunk.js"
+  },
+  {
+    "revision": "a8142c892c46c2bd95a6",
+    "url": "/zevocorporation/Writer-App.git/static/js/runtime-main.fd59a5c2.js"
+  },
+  {
+    "revision": "bc5c1cefd5e0cec50cac9cf31f02fcd5",
+    "url": "/zevocorporation/Writer-App.git/static/media/error.bc5c1cef.svg"
+  },
+  {
+    "revision": "9491b6f3a4593d7ae1c57c99867fb0cd",
+    "url": "/zevocorporation/Writer-App.git/static/media/logo.9491b6f3.svg"
+  },
+  {
+    "revision": "5c25d78b6954b7fb6d803b6e322b7e33",
+    "url": "/zevocorporation/Writer-App.git/static/media/success.5c25d78b.svg"
+  },
+  {
+    "revision": "c39ec7093583372dfc32e804f65ef071",
+    "url": "/zevocorporation/Writer-App.git/static/media/warn.c39ec709.svg"
+  }
+]);
